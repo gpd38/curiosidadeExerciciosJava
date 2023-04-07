e89efe1867c2db7ca6331b3a3fe5d88c581cccc3
gpd38 <gustavodias2@gmail.com>
2023-04-06 22:34:05 -0300

    Inserindo código de leitura de arquivo utilizando scanner

--- codigosAleatorios/src/main/java/leituraDeArquivo/ReadWithScanner.java
+++ codigosAleatorios/src/main/java/leituraDeArquivo/ReadWithScanner.java
@@ -8,7 +8,7 @@ public class ReadWithScanner {
 
 	public static void main(String[] args) {
 
-		File file = new File("C:\\temp\\in.txt");
+		File file = new File("C:\\temp\\input.txt");
 		Scanner sc = null;
 		try {
 			sc = new Scanner(file);
