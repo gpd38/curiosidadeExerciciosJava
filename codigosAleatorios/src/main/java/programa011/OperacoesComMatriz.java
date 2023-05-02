76d883a15425766ea11b4eac178a8d0beb7f1a8c
Gustavo Pereira Dias <gustavo.dias@adiq.com.br>
2023-05-02 17:52:51 -0300

    Atualizando código do programa 011: Operações com matriz

--- codigosAleatorios/src/main/java/programa011/OperacoesComMatriz.java
+++ codigosAleatorios/src/main/java/programa011/OperacoesComMatriz.java
@@ -9,49 +9,47 @@ public class OperacoesComMatriz {
 		Scanner scanner = new Scanner(System.in);
 		int opcao = -1;
 
-		while (opcao != 0 || opcao > 34) {
-			System.out.println("Escolha uma das opções abaixo:");
+		while (opcao != 0 || opcao > 32) {
+			System.out.println("Escolha uma das opcoes abaixo:");
 			System.out.println("00. Sair    ");
-			System.out.println("01. Opção 01");
-			System.out.println("02. Opção 02");
-			System.out.println("03. Opção 03");
-			System.out.println("04. Opção 04");
-			System.out.println("05. Opção 05");
-			System.out.println("06. Opção 06");
-			System.out.println("07. Opção 07");
-			System.out.println("08. Opção 08");
-			System.out.println("09. Opção 09");
-			System.out.println("10. Opção 10");
-			System.out.println("11. Opção 11");
-			System.out.println("12. Opção 12");
-			System.out.println("13. Opção 13");
-			System.out.println("14. Opção 14");
-			System.out.println("15. Opção 15");
-			System.out.println("16. Opção 16");
-			System.out.println("17. Opção 17");
-			System.out.println("18. Opção 18");
-			System.out.println("19. Opção 19");
-			System.out.println("20. Opção 20");
-			System.out.println("21. Opção 21");
-			System.out.println("22. Opção 22");
-			System.out.println("23. Opção 23");
-			System.out.println("24. Opção 24");
-			System.out.println("25. Opção 25");
-			System.out.println("26. Opção 26");
-			System.out.println("27. Opção 27");
-			System.out.println("28. Opção 28");
-			System.out.println("29. Opção 29");
-			System.out.println("30. Opção 30");
-			System.out.println("31. Opção 31");
-			System.out.println("32. Opção 32");
-			System.out.println("33. Opção 33");
-			System.out.println("34. Opção 34");
-			System.out.print("Opção escolhida: ");
+			System.out.println("01. Opcao 01");
+			System.out.println("02. Opcao 02");
+			System.out.println("03. Opcao 03");
+			System.out.println("04. Opcao 04");
+			System.out.println("05. Opcao 05");
+			System.out.println("06. Opcao 06");
+			System.out.println("07. Opcao 07");
+			System.out.println("08. Opcao 08");
+			System.out.println("09. Opcao 09");
+			System.out.println("10. Opcao 10");
+			System.out.println("11. Opcao 11");
+			System.out.println("12. Opcao 12");
+			System.out.println("13. Opcao 13");
+			System.out.println("14. Opcao 14");
+			System.out.println("15. Opcao 15");
+			System.out.println("16. Opcao 16");
+			System.out.println("17. Opcao 17");
+			System.out.println("18. Opcao 18");
+			System.out.println("19. Opcao 19");
+			System.out.println("20. Opcao 20");
+			System.out.println("21. Opcao 21");
+			System.out.println("22. Opcao 22");
+			System.out.println("23. Opcao 23");
+			System.out.println("24. Opcao 24");
+			System.out.println("25. Opcao 25");
+			System.out.println("26. Opcao 26");
+			System.out.println("27. Opcao 27");
+			System.out.println("28. Opcao 28");
+			System.out.println("29. Opcao 29");
+			System.out.println("30. Opcao 30");
+			System.out.println("31. Opcao 31");
+			System.out.println("32. Opcao 32");
+			System.out.print("Opcao escolhida: ");
 
 			try {
 				opcao = scanner.nextInt();
 			} catch (Exception e) {
-				System.out.println("Opção inválida! Digite um número entre 1 e 35.");
+				System.out.println("Opcao invalida! Digite um numero entre 1 e 32.");
 				scanner.nextLine();
 				continue;
 			}
@@ -62,109 +60,109 @@ public class OperacoesComMatriz {
 				System.exit(0);
 				break;
 			case 1:
-				// Realizar ação da opção 1
+				// Realizar aï¿½ï¿½o da opï¿½ï¿½o 1
 				break;
 			case 2:
-				// Realizar ação da opção 2
+				// Realizar aï¿½ï¿½o da opï¿½ï¿½o 2
 				break;
 			case 3:
-				// Realizar ação da opção 3
+				// Realizar aï¿½ï¿½o da opï¿½ï¿½o 3
 				break;
 			case 4:
-				// Realizar ação da opção 4
+				// Realizar aï¿½ï¿½o da opï¿½ï¿½o 4
 				break;
 			case 5:
-				// Realizar ação da opção 5
+				// Realizar aï¿½ï¿½o da opï¿½ï¿½o 5
 				break;
 			case 6:
-				// Realizar ação da opção 6
+				// Realizar aï¿½ï¿½o da opï¿½ï¿½o 6
 				break;
 			case 7:
-				// Realizar ação da opção 7
+				// Realizar aï¿½ï¿½o da opï¿½ï¿½o 7
 				break;
 			case 8:
-				// Realizar ação da opção 8
+				// Realizar aï¿½ï¿½o da opï¿½ï¿½o 8
 				break;
 			case 9:
-				// Realizar ação da opção 9
+				// Realizar aï¿½ï¿½o da opï¿½ï¿½o 9
 				break;
 			case 10:
-				// Realizar ação da opção 10
+				// Realizar aï¿½ï¿½o da opï¿½ï¿½o 10
 				break;
 			case 11:
-				// Realizar ação da opção 11
+				// Realizar aï¿½ï¿½o da opï¿½ï¿½o 11
 				break;
 			case 12:
-				// Realizar ação da opção 12
+				// Realizar aï¿½ï¿½o da opï¿½ï¿½o 12
 				break;
 			case 13:
-				// Realizar ação da opção 13
+				// Realizar aï¿½ï¿½o da opï¿½ï¿½o 13
 				break;
 			case 14:
-				// Realizar ação da opção 14
+				// Realizar aï¿½ï¿½o da opï¿½ï¿½o 14
 				break;
 			case 15:
-				// Realizar ação da opção 15
+				// Realizar aï¿½ï¿½o da opï¿½ï¿½o 15
 				break;
 			case 16:
-				// Realizar ação da opção 16
+				// Realizar aï¿½ï¿½o da opï¿½ï¿½o 16
 				break;
 			case 17:
-				// Realizar ação da opção 17
+				// Realizar aï¿½ï¿½o da opï¿½ï¿½o 17
 				break;
 			case 18:
-				// Realizar ação da opção 18
+				// Realizar aï¿½ï¿½o da opï¿½ï¿½o 18
 				break;
 			case 19:
-				// Realizar ação da opção 19
+				// Realizar aï¿½ï¿½o da opï¿½ï¿½o 19
 				break;
 			case 20:
-				// Realizar ação da opção 20
+				// Realizar aï¿½ï¿½o da opï¿½ï¿½o 20
 				break;
 			case 21:
-				// Realizar ação da opção 21
+				// Realizar aï¿½ï¿½o da opï¿½ï¿½o 21
 				break;
 			case 22:
-				// Realizar ação da opção 22
+				// Realizar aï¿½ï¿½o da opï¿½ï¿½o 22
 				break;
 			case 23:
-				// Realizar ação da opção 23
+				// Realizar aï¿½ï¿½o da opï¿½ï¿½o 23
 				break;
 			case 24:
-				// Realizar ação da opção 24
+				// Realizar aï¿½ï¿½o da opï¿½ï¿½o 24
 				break;
 			case 25:
-				// Realizar ação da opção 25
+				// Realizar aï¿½ï¿½o da opï¿½ï¿½o 25
 				break;
 			case 26:
-				// Realizar ação da opção 26
+				// Realizar aï¿½ï¿½o da opï¿½ï¿½o 26
 				break;
 			case 27:
-				// Realizar ação da opção 27
+				// Realizar aï¿½ï¿½o da opï¿½ï¿½o 27
 				break;
 			case 28:
-				// Realizar ação da opção 28
+				// Realizar aï¿½ï¿½o da opï¿½ï¿½o 28
 				break;
 			case 29:
-				// Realizar ação da opção 29
+				// Realizar aï¿½ï¿½o da opï¿½ï¿½o 29
 				break;
 			case 30:
-				// Realizar ação da opção 30
+				// Realizar aï¿½ï¿½o da opï¿½ï¿½o 30
 				break;
 			case 31:
-				// Realizar ação da opção 31
+				// Realizar aï¿½ï¿½o da opï¿½ï¿½o 31
 				break;
 			case 32:
-				// Realizar ação da opção 32
+				// Realizar aï¿½ï¿½o da opï¿½ï¿½o 32
 				break;
 			case 33:
-				// Realizar ação da opção 33
+				// Realizar aï¿½ï¿½o da opï¿½ï¿½o 33
 				break;
 			case 34:
-				// Realizar ação da opção 34
+				// Realizar aï¿½ï¿½o da opï¿½ï¿½o 34
 				break;
 			default:
-				System.out.println("Digitou um valor inválido.\nPrograma sendo finalizado!");
+				System.out.println("Digitou um valor invalido.\nPrograma sendo finalizado!");
 				System.exit(0);
 				break;
 			}
