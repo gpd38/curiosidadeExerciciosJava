523701f04506905f6c5c13d5ec45a3c4299c5976
gpd38 <gustavodias2@gmail.com>
2023-05-02 23:40:13 -0300

    Removendo lixo do código principal do programa011: Operação de matrizes

--- codigosAleatorios/src/main/java/programa011/OperacoesComMatriz.java
+++ codigosAleatorios/src/main/java/programa011/OperacoesComMatriz.java
@@ -60,106 +60,68 @@ public class OperacoesComMatriz {
 				System.exit(0);
 				break;
 			case 1:
-				// Realizar a��o da op��o 1
 				break;
 			case 2:
-				// Realizar a��o da op��o 2
 				break;
 			case 3:
-				// Realizar a��o da op��o 3
 				break;
 			case 4:
-				// Realizar a��o da op��o 4
 				break;
 			case 5:
-				// Realizar a��o da op��o 5
 				break;
 			case 6:
-				// Realizar a��o da op��o 6
 				break;
 			case 7:
-				// Realizar a��o da op��o 7
 				break;
 			case 8:
-				// Realizar a��o da op��o 8
 				break;
 			case 9:
-				// Realizar a��o da op��o 9
 				break;
 			case 10:
-				// Realizar a��o da op��o 10
 				break;
 			case 11:
-				// Realizar a��o da op��o 11
 				break;
 			case 12:
-				// Realizar a��o da op��o 12
 				break;
 			case 13:
-				// Realizar a��o da op��o 13
 				break;
 			case 14:
-				// Realizar a��o da op��o 14
 				break;
 			case 15:
-				// Realizar a��o da op��o 15
 				break;
 			case 16:
-				// Realizar a��o da op��o 16
 				break;
 			case 17:
-				// Realizar a��o da op��o 17
 				break;
 			case 18:
-				// Realizar a��o da op��o 18
 				break;
 			case 19:
-				// Realizar a��o da op��o 19
 				break;
 			case 20:
-				// Realizar a��o da op��o 20
 				break;
 			case 21:
-				// Realizar a��o da op��o 21
 				break;
 			case 22:
-				// Realizar a��o da op��o 22
 				break;
 			case 23:
-				// Realizar a��o da op��o 23
 				break;
 			case 24:
-				// Realizar a��o da op��o 24
 				break;
 			case 25:
-				// Realizar a��o da op��o 25
 				break;
 			case 26:
-				// Realizar a��o da op��o 26
 				break;
 			case 27:
-				// Realizar a��o da op��o 27
 				break;
 			case 28:
-				// Realizar a��o da op��o 28
 				break;
 			case 29:
-				// Realizar a��o da op��o 29
 				break;
 			case 30:
-				// Realizar a��o da op��o 30
 				break;
 			case 31:
-				// Realizar a��o da op��o 31
 				break;
 			case 32:
-				// Realizar a��o da op��o 32
-				break;
-			case 33:
-				// Realizar a��o da op��o 33
-				break;
-			case 34:
-				// Realizar a��o da op��o 34
 				break;
 			default:
 				System.out.println("Digitou um valor invalido.\nPrograma sendo finalizado!");
